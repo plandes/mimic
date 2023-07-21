6de6032ab208da69f41ed99511975599cae23ec9
Paul Landes <landes@mailc.net>
2023-07-21 12:25:56 -0500

    cli: add admission dump; cleanup CLI

--- src/python/zensols/mimic/app.py
+++ src/python/zensols/mimic/app.py
@@ -9,12 +9,12 @@ from enum import Enum, auto
 import logging
 from pathlib import Path
 import re
-from zensols.persist import Stash
+from zensols.persist import FileTextUtil
 from zensols.config import ConfigFactory
 from zensols.cli import ApplicationError
 from zensols.nlp import FeatureDocumentParser, FeatureDocument, FeatureToken
 from . import (
-    NoteEvent, NoteEventPersister,
+    NoteEvent, NoteEventPersister, Note,
     HospitalAdmission, HospitalAdmissionDbStash, Corpus,
 )
 
@@ -25,6 +25,10 @@ class _Format(Enum):
     meta = auto()
     txt = auto()
     json = auto()
+
+    @property
+    def ext(self) -> str:
+        return 'txt' if self.name == 'meta' else self.name
 
 
 @dataclass
@@ -85,18 +89,51 @@ class Application(object):
         """Clear the all cached admission and note parses."""
         self.corpus.clear()
 
-    def write_admission(self, hadm_id: str):
-        """Write a discharge summary.
-
-        :param hadm_id: the hospital admission ID
-
-        """
+    def _get_adm(self, hadm_id: str) -> HospitalAdmission:
         stash: HospitalAdmissionDbStash = self.corpus.hospital_adm_stash
-        if hadm_id is None:
+        if hadm_id == '-':
             adm = next(iter(stash.values()))
         else:
             adm = stash[str(hadm_id)]
+        return adm
+
+    def write_admission_summary(self, hadm_id: str):
+        """Write an admission note categories and section names.
+
+        :param hadm_id: the hospital admission ID or ``-`` for a random ID
+
+        """
+        adm: HospitalAdmission = self._get_adm(hadm_id)
         adm.write()
+
+    def write_admission(self, hadm_id: str, out_dir: Path = Path('.'),
+                        output_format: _Format = _Format.meta):
+        """Write all the notes of an admission.
+
+        :param hadm_id: the hospital admission ID or ``-`` for a random ID
+
+        :param out_dir: the output directory
+
+        :param output_format: the output format of the note
+
+        """
+        if hadm_id == '-':
+            stash: HospitalAdmissionDbStash = self.corpus.hospital_adm_stash
+            hadm_id = next(iter(stash.keys()))
+        adm: HospitalAdmission = self._get_adm(hadm_id)
+        out_dir = out_dir / 'adm' / hadm_id
+        out_dir.mkdir(parents=True, exist_ok=True)
+        note: Note
+        for note in adm.notes:
+            name: str = FileTextUtil.normalize_text(
+                f'{note.category}-{note.description}')
+            path: Path = out_dir / f'{note.row_id}-{name}.{output_format.ext}'
+            logger.info(f'wrote note to {path}')
+            with open(path, 'w') as f:
+                {_Format.meta: lambda: note.write_human(writer=f),
+                 _Format.txt: lambda: f.write(note.text),
+                 _Format.json: lambda: f.write(note.asjson(indent=4)),
+                 }[output_format]()
 
     def write_note(self, row_id: str):
         """Write a note.
@@ -125,10 +162,12 @@ class Application(object):
             'temporary_results_dir', 'mimic_default'))
 
     def write_note_by_categories(self, note_limit: int = 1,
-                                 output_format: _Format = _Format.txt):
+                                 output_format: _Format = _Format.meta):
         """Write a certain number of notes across each category.
 
         :param note_limit: the number of notes to write
+
+        :param output_format: the output format of the note
 
         """
         np: NoteEventPersister = self.corpus.note_event_persister
@@ -138,13 +177,13 @@ class Application(object):
         ntevt_cnt = 0
         for i, cat in enumerate(np.categories):
             ntevts: Tuple[NoteEvent] = np.get_notes_by_category(cat, note_limit)
-            name = re.sub(r'[ \t/_-]+', '-', cat).lower()
+            name: str = re.sub(r'[ \t/_-]+', '-', cat).lower()
             if name.endswith('-'):
                 name = name[0:-1]
             for ntevt in ntevts:
-                path = tmpdir / f'{name}-{ntevt.hadm_id}.{output_format.name}'
+                path = tmpdir / f'{name}-{ntevt.hadm_id}.{output_format.meta}'
                 with open(path, 'w') as f:
-                    {_Format.meta: lambda: ntevt.write(writer=f),
+                    {_Format.meta: lambda: ntevt.write_human(writer=f),
                      _Format.txt: lambda: f.write(ntevt.text),
                      _Format.json: lambda: f.write(ntevt.asjson(indent=4)),
                      }[output_format]()
@@ -154,44 +193,44 @@ class Application(object):
         if logger.isEnabledFor(logging.INFO):
             logger.info(f'wrote {ntevt_cnt} ntevts')
 
-    def write_discharge_summaries(self, note_limit: int = 1):
-        """Write discharge notes to the file system.
+    def write_discharge_reports(self, note_limit: int = 1,
+                                out_dir: Path = Path('.')):
+        """Write discharge reports (as apposed to addendums).
 
         :param note_limit: the number of notes to write
 
+        :param out_dir: the output directory
+
         """
         np: NoteEventPersister = self.corpus.note_event_persister
-        tmpdir: Path = self._get_temporary_results_dir() / 'discharge-reports'
-        tmpdir.mkdir(parents=True, exist_ok=True)
+        out_dir.mkdir(parents=True, exist_ok=True)
         notes: Tuple[NoteEvent] = np.get_discharge_reports(note_limit)
         for note in notes:
-            path = tmpdir / f'{note.hadm_id}.txt'
+            path = out_dir / f'{note.hadm_id}.txt'
             with open(path, 'w') as f:
                 note.write(writer=f)
         if logger.isEnabledFor(logging.INFO):
-            logger.info(f'wrote {len(notes)} notes to {tmpdir}')
+            logger.info(f'wrote {len(notes)} notes to {out_dir}')
 
     def write_note_categories(self, hadm_id: str):
         """Write note categories of an admission.
 
-        :param hadm_id: the hospital admission ID
+        :param hadm_id: the hospital admission ID or ``-`` for a random ID
 
         """
-        stash: Stash = self.corpus.hospital_adm_stash
-        adm: HospitalAdmission = stash[hadm_id]
+        adm: HospitalAdmission = self._get_adm(hadm_id)
         for note in adm:
             print(f'{note.row_id},{note.category}')
 
     def _unmatched_tokens(self, hadm_id: str, no_ents: bool = False):
         """Find all unmatched tokens for an admission.
 
-        :param hadm_id: the hospital admission ID
+        :param hadm_id: the hospital admission ID or ``-`` for a random ID
 
         :param no_ents: do not include unmatched entities
 
         """
-        stash: Stash = self.corpus.hospital_adm_stash
-        adm: HospitalAdmission = stash[hadm_id]
+        adm: HospitalAdmission = self._get_adm(hadm_id)
         for note in adm.notes:
             print(note)
             norm = note.doc.norm
